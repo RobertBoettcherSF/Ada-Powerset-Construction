507eef6b65c73aa06873fdb1ccad5f6463970da0
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-31 07:35:28 +0200

    Refactor hash functions and transition types

--- powerset_construction.ads
+++ powerset_construction.ads
@@ -1,27 +1,26 @@
 --  Powerset Construction Algorithm Specification
 --  Converts an NFA (including ε-NFA) to an equivalent DFA.
 
-with Ada.Containers.Vectors;
 with Ada.Containers.Hashed_Sets;
+use Ada.Containers;
 
 package Powerset_Construction is
-   pragma Pure;
 
    -- Custom types for the algorithm domain
    type State_Type is range 0 .. Integer'Last;
    type Symbol_Type is range 0 .. Integer'Last;
 
    -- For sets of states (DFA states are sets of NFA states)
-   function State_Hash (S : State_Type) return Ada.Containers.Hash_Type;
-   package State_Sets is new Ada.Containers.Hashed_Sets
+   function State_Hash (S : State_Type) return Hash_Type;
+   package State_Sets is new Hashed_Sets
      (Element_Type        => State_Type,
       Hash                => State_Hash,
       Equivalent_Elements => "=");
    subtype State_Set is State_Sets.Set;
 
    -- For sets of symbols (alphabet)
-   function Symbol_Hash (S : Symbol_Type) return Ada.Containers.Hash_Type;
-   package Symbol_Sets is new Ada.Containers.Hashed_Sets
+   function Symbol_Hash (S : Symbol_Type) return Hash_Type;
+   package Symbol_Sets is new Hashed_Sets
      (Element_Type        => Symbol_Type,
       Hash                => Symbol_Hash,
       Equivalent_Elements => "=");
@@ -32,13 +31,14 @@ package Powerset_Construction is
    type Transition_Map_Access is access Transition_Map;
 
    -- Transition array: from state to transition map
-   type Transition_Array_Type is array (State_Type range <>) of Transition_Map_Access;
+   type Transition_Array is array (State_Type range <>) of Transition_Map_Access;
+   type Transition_Array_Access is access Transition_Array;
 
    -- NFA and DFA types
    type NFA_Type is record
       States      : State_Set;
       Alphabet    : Symbol_Set;
-      Transitions : Transition_Array_Type;
+      Transitions : Transition_Array_Access;
       Initial     : State_Set;
       Accepting   : State_Set;
    end record;
@@ -46,7 +46,7 @@ package Powerset_Construction is
    type DFA_Type is record
       States      : State_Set;
       Alphabet    : Symbol_Set;
-      Transitions : Transition_Array_Type;
+      Transitions : Transition_Array_Access;
       Initial     : State_Type;
       Accepting   : State_Set;
    end record;
@@ -68,12 +68,16 @@ package Powerset_Construction is
    -- Helper: Compute ε-closure for a set of states
    function Epsilon_Closure (NFA : NFA_Type; States : State_Set) return State_Set;
 
+   -- Helper: Compute the next state set for a given state set and symbol
+   function Next_State_Set
+     (NFA : NFA_Type; Current : State_Set; Symbol : Symbol_Type) return State_Set;
+
 private
    -- Hash functions for State_Type and Symbol_Type
-   function State_Hash (S : State_Type) return Ada.Containers.Hash_Type is
-     (Ada.Containers.Hash_Type(S));
+   function State_Hash (S : State_Type) return Hash_Type is
+     (Hash_Type(S));
 
-   function Symbol_Hash (S : Symbol_Type) return Ada.Containers.Hash_Type is
-     (Ada.Containers.Hash_Type(S));
+   function Symbol_Hash (S : Symbol_Type) return Hash_Type is
+     (Hash_Type(S));
 
 end Powerset_Construction;
